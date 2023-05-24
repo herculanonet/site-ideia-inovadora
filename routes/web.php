5f78461a5a55d8d1edd773c02ff27207aa5cc533
Herculano <130322540+herculanonet@users.noreply.github.com>
2023-05-23 22:47:23 -0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,6 @@ Route::group(['namespace' => 'App\Http\Controllers'], function()
     Route::get('/about', 'PagesController@about')->name('pages.about');
     Route::get('/pricing', 'PagesController@pricing')->name('pages.pricing');
     Route::get('/faqs', 'PagesController@faqs')->name('pages.faqs');
-
     Route::post('/login', 'store')->name('login.store');
     Route::get('/logout', 'destroy')->name('login.destroy');
-
 });
